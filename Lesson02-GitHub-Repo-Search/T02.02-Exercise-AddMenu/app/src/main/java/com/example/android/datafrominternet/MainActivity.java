c0915a2e5143cef91fba8ecdf8f5c72ca4374e66
Samone <samonemorris@gmail.com>
2018-03-05 20:14:17 -0600

    Exercise T02.02 : Created a new Layout file main.xml to hold our ActionBar element 'Search' to search for queries when clicked

--- Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -39,23 +39,22 @@ public class MainActivity extends AppCompatActivity {
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
     }
 
-    // Do 2 - 7 in menu.xml ///////////////////////////////////////////////////////////////////////
-    // TODO (2) Create a menu xml called 'main.xml' in the res->menu folder
-    // TODO (3) Add one menu item to your menu
-    // TODO (4) Give the menu item an id of @+id/action_search
-    // TODO (5) Set the orderInCategory to 1
-    // TODO (6) Show this item if there is room (use app:showAsAction, not android:showAsAction)
-    // TODO (7) Set the title to the search string ("Search") from strings.xml
-    // Do 2 - 7 in menu.xml ///////////////////////////////////////////////////////////////////////
-
+    // Do 2 - 7 in main.xml ///////////////////////////////////////////////////////////////////////
+    // COMPLETED (2) Create a main xml called 'main.xml' in the res->main folder
+    // COMPLETED (3) Add one main item to your main
+    // COMPLETED (4) Give the main item an id of @+id/action_search
+    // COMPLETED (5) Set the orderInCategory to 1
+    // COMPLETED (6) Show this item if there is room (use app:showAsAction, not android:showAsAction)
+    // COMPLETED (7) Set the title to the search string ("Search") from strings.xml
+    // Do 2 - 7 in main.xml ///////////////////////////////////////////////////////////////////////
 
     // TODO (8) Override onCreateOptionsMenu
-    // TODO (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
-    // TODO (10) Return true to display your menu
+    // TODO (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the main
+    // TODO (10) Return true to display your main
 
     // TODO (11) Override onOptionsItemSelected
     // TODO (12) Within onOptionsItemSelected, get the ID of the item that was selected
-    // TODO (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
+    // TODO (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this main click
     // TODO (14) Don't forgot to call .show() on your Toast
-    // TODO (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
+    // TODO (15) If you do NOT handle the main click, return super.onOptionsItemSelected to let Android handle the main click
 }
