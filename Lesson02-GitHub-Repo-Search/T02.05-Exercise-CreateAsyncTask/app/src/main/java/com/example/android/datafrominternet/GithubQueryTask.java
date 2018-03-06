98eb3e3611240c8b501514ed9787109448b89e1a
Samone <samonemorris@gmail.com>
2018-03-06 14:24:11 -0600

    Exercise T02.05 : Created a new AsyncTask class, GithubQueryTask

---
+++ Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/GithubQueryTask.java
@@ -0,0 +1,16 @@
+package com.example.android.datafrominternet;
+
+import android.os.AsyncTask;
+
+import java.net.URL;
+
+/**
+ * Created by Samone on 3/6/2018.
+ */
+
+public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+    @Override
+    protected String doInBackground(URL... urls) {
+        return null;
+    }
+}
