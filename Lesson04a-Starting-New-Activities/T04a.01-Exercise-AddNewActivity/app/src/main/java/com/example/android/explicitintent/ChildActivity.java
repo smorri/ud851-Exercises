fc429673aa5bd52579e1c36f3468c2f1b9d06185
Samone <samonemorris@gmail.com>
2018-03-10 20:13:40 -0600

    Exercise T04a.01 : Applied changes to the activity_child layout file. Added references to the layout components in ChildActivity class

--- Lesson04a-Starting-New-Activities/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -2,12 +2,16 @@ package com.example.android.explicitintent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ChildActivity extends AppCompatActivity {
+    private TextView display_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
+
+        display_message = (TextView) findViewById( R.id.tv_display );
     }
 }
