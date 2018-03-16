ffbfde44c7aee84229dc5510879ae982a1c0d61b
Samone <samonemorris@gmail.com>
2018-03-15 19:50:58 -0500

    Exercise T06.02 : Created a fragment called SettingsFragment. Created a layout file to hold the preference options called pref_visualizer.xml

---
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -0,0 +1,18 @@
+package android.example.com.visualizerpreferences;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+
+
+/**
+ * A simple {@link PreferenceFragmentCompat} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }// end onCreatePreferences(...)
+}// end class SettingsFragment
