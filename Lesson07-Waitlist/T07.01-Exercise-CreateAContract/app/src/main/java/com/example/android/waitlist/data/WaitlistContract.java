9b43269106baad301051fa799b244b157893bf6e
Samone <samonemorris@gmail.com>
2018-04-09 15:08:00 -0500

    Exercise T07.01 : Within the WaitlistContract class, created an inner class which will define the Waitlist Table, and the columns associated within this table.

--- Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -2,16 +2,24 @@ package com.example.android.waitlist.data;
 
 import android.provider.BaseColumns;
 
+/**
+ * SQLite Waitlist : Contract Class
+ *
+ * @author Samone Morris
+ * @date   04/09/18 (Re-completed as original file was lost due to motherboard failure)
+ */
+
 public class WaitlistContract {
+    // COMPLETED (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
 
+    // To prevent users from creating an instance of this class
+    private WaitlistContract(){}
 
-
-    // TODO (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
-
-        // TODO (2) Inside create a static final members for the table name and each of the db columns
-        // TABLE_NAME -> waitlist;
-        // COLUMN_GUEST_NAME -> guestName
-        // COLUMN_PARTY_SIZE -> partySize
-        // COLUMN_TIMESTAMP -> timestamp
-
+    public final static class WaitlistEntry implements BaseColumns{
+        // COMPLETED (2) Inside create a static final members for the table name and each of the db columns :
+        public final static String TABLE_NAME = "waitlist",         // TABLE_NAME -> waitlist;
+                                   COLUMN_GUEST_NAME = "guestName", // COLUMN_GUEST_NAME -> guestName
+                                   COLUMN_PARTY_SIZE = "partySize", // COLUMN_PARTY_SIZE -> partySize
+                                   COLUMN_TIMESTAMP = "timestamp";  // COLUMN_TIMESTAMP -> timestamp
+    }// end inner class WaitlistEntry
 }
